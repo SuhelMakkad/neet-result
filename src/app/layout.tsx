dbc3cfb32d748e994127e0a389ce3c2c2c5dc2ab
Quarticdot AI <makadsuhel11@gmail.com>
2024-06-13 09:10:43 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NEET Results",
+  description: "Check NEET results",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
